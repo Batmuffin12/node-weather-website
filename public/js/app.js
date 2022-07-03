75a49e7437b816dba993b67ed3cad721eafa2a72
Ofek Bs <ofekbensimhon.work@gmail.com>
2022-07-03 15:08:59 +0300

    debugging fetch

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit', (e) =>{
     messageOne.textContent = 'loading'
     messageTwo.textContent = ''
 
-    fetch(`/weather?address=${location}`).then((response) =>{
+    fetch(`weather?address=${location}`).then((response) =>{
         response.json().then((data) =>{
             if(data.error){
                 messageOne.textContent = 'ERROR'
